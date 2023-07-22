de7cda2f54008893a773f527ed574ddb4a04ed27
Shouvik <deshouvik078@gmail.com>
2023-07-22 07:59:39 +0530

    new

---
+++ PY10/five2one.py
@@ -0,0 +1,13 @@
+n=5
+for x in range(n,0,-1):
+    for y in range(n,0,-1):
+        print(x,end=" ")
+    print()
+
+'''
+5 5 5 5 5 
+4 4 4 4 4
+3 3 3 3 3
+2 2 2 2 2
+1 1 1 1 1
+'''
---
+++ PY10/five2one2.py
@@ -0,0 +1,5 @@
+n=5
+for x in range(n,0,-1):
+    for y in range(n,0,-1):
+        print(y,end=" ")
+    print()
