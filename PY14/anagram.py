aa29b7f385023d0c1be2c521df0209927b28806c
Shouvik <deshouvik078@gmail.com>
2023-07-26 06:25:27 +0530

    format

---
+++ PY14/anagram.py
@@ -0,0 +1,7 @@
+s1=input("Enter a string ")
+s2=input("Enter a string ")
+
+if sorted(s1)==sorted(s2):
+    print("The strings are anagrams ")
+else :
+    print("The strings are not anagrams ")
---
+++ PY15/formatc.py
@@ -0,0 +1,4 @@
+n=int(input("Enter your age : "))
+name=input("Enter your name : ")
+
+print("My name is {} and age is {}".format(name,n))
