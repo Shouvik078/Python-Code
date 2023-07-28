8c123f14d33cc5c575ca20cc7fe253e67fe3a9a9
Shouvik <deshouvik078@gmail.com>
2023-07-28 23:39:00 +0530

    and

--- PY16/rotatearray.py
+++ PY16/rotatearray.py
@@ -1,3 +1,10 @@
 # Input: nums = [1,2,3,4,5,6,7], k = 3
 # Output: [5,6,7,1,2,3,4]
 
+# result
+
+# class Solution:
+#     def rotate(self, nums: List[int], k: int) -> None:
+#         for i in range(k):
+#             a=nums.pop()
+#             nums.insert(0,a)
---
+++ PY16/vote.py
@@ -0,0 +1,7 @@
+n=int(input("Enter age : "))
+s=input("Enter Gender :")
+
+if n>=18 and s=='M':
+    print("Vote")
+else:
+    print("No Vote")
