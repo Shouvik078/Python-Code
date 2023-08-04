7d82ab11d5699dfb4d68cc8975d9e6427edf90ce
Shouvik <deshouvik078@gmail.com>
2023-08-04 20:24:17 +0530

    Matrix Search

---
+++ PY20/sqrt.py
@@ -0,0 +1,19 @@
+# Matrix element searching 
+# Python3 code to demonstrate
+# Search in Matrix
+# using any() + list comprehension
+
+# initializing list
+test_list = [[4, 5, 6],
+			[10, 2, 13],
+			[1, 11, 18]]
+
+# printing original list
+print("The original list : " + str(test_list))
+
+# using any() + list comprehension
+# to Search in Matrix
+res = any(13 in sub for sub in test_list)
+
+# printing result
+print("Is 13 present in Matrix ? : " + str(res))
